73c0c3129fa2dcb22ef7892ae2d6f116fb4d8039
AppleCrazy <applecrazy@users.noreply.github.com>
2015-11-03 10:30:56 -0800

    Create RubeGoldbergServer.java

---
+++ RubeGoldbergServer.java
@@ -0,0 +1,8 @@
+package BerbawyMakers;
+import java.net.*;
+import java.io.*;
+public class RubeGoldbergServer {
+    public static void main(String[] args) {
+        System.out.println("RUDEGOLDBERGSERVER\n-----------------\nDeveloped by Berbawy Makers and the other group.");
+    }
+}
