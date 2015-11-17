51f6f6580e543239e2dea1ae41cb692a1658c57f
AppleCrazy <applecrazy@users.noreply.github.com>
2015-11-17 10:31:07 -0800

    Hard-coded IP of my machine

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket               # Import socket module
 
 s = socket.socket()         # Create a socket object
-host = "10.20.22.22"     # Remote machine IP
+host = "10.20.22.22"     # Hard coded to my 101A machine.
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
