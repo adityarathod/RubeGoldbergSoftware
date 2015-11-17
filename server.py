2ef3ccfb185bb44c3f2325e56494efea5800a4ce
AppleCrazy <applecrazy@users.noreply.github.com>
2015-11-17 10:30:32 -0800

    Added Berbawy Makers "propaganda"

--- server.py
+++ server.py
@@ -9,5 +9,5 @@ s.listen(5)                 # Now wait for client connection.
 while True:
    c, addr = s.accept()     # Establish connection with client.
    print ('Got connection from', addr)
-   c.send('Thank you for connecting'.encode())
+   c.send('Berbawy's Makers is epic.'.encode())
    c.close()                # Close the connection
