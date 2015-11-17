c6674c2b370c2bd47f04bbc1f675010e2679d4da
AppleCrazy <applecrazy@users.noreply.github.com>
2015-11-17 10:19:41 -0800

    Delete RubeGoldbergServer.java

--- RubeGoldbergServer.java
+++
@@ -1,7 +0,0 @@
-import java.net.*;
-import java.io.*;
-public class RubeGoldbergServer {
-    public static void main(String[] args) {
-        System.out.println("RUDEGOLDBERGSERVER\n-----------------\nDeveloped by Berbawy Makers and the other group.");
-    }
-}
