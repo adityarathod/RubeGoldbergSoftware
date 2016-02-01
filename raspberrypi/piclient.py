258549d5e3f94dab9460f1869ff72f46697d4f4d
AppleCrazy <applecrazy@users.noreply.github.com>
2016-02-01 10:33:26 -0800

    Added code, still a WIP

--- raspberrypi/piclient.py
+++ raspberrypi/piclient.py
@@ -1,1 +1,11 @@
+import socket
 
+s = socket.socket()
+# Localhost for testing purposes
+host = "127.0.0.1"
+# Need to figure out what port all school networks DON'T block.
+port = 12345
+
+s.connect((host, port))
+print (s.recv(1024).decode())
+s.close
