e990304648e446afadda377aef4cc8b7268b5828
AppleCrazy <applecrazy@users.noreply.github.com>
2016-02-10 12:37:20 -0800

    Create piserver.py

---
+++ raspberrypi/piserver.py
@@ -0,0 +1,14 @@
+import socket               # Import socket module
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 12345                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print ('Connected to ', addr)
+   c.send('GO'.encode())
+   print "Sent signal to " + addr + "."
+   c.close()                # Close the connection
