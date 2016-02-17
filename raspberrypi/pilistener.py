07f277bd9fdd512a424d50724926eae84f8160c1
AppleCrazy <archronicle7@gmail.com>
2016-02-17 18:26:17 +0000

    Added more user-friendly messages.

--- raspberrypi/pilistener.py
+++ raspberrypi/pilistener.py
@@ -1,16 +1,17 @@
 import socket               # Import socket module
 
+def move():
+   print "moved."
 s = socket.socket()         # Create a socket object
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Tells the system to okay the reuse of a port
 host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
+port = 238                # Reserve a port for your service.
 s.bind((host, port))        # Bind to the port
 
 s.listen(5)                 # Now wait for client connection.
-print ("Welcome to the Rube Goldberg Software!\nWaiting for connection...")
+print "Welcome to the Rube Goldberg Software!\nWaiting for connection on " + host + ":" + str(port) +"..."
 while True:
    c, addr = s.accept()     # Establish connection with client.
-   print ('Recieved signal from ', addr)
+   print 'Recieved signal from ', addr
    move()
    c.close()                # Close the connection
-def move():
-   print("Moved.")
--- raspberrypi/pisender.py
+++ raspberrypi/pisender.py
@@ -1,12 +1,14 @@
 import os
+
 import socket
 s = socket.socket()
 # Localhost for testing purposes
-host = "127.0.0.1"
+host = socket.gethostname() #"127.0.0.1"
 # Need to figure out what port all school networks DON'T block.
-port = 12345
+port = 238
 print "Welcome to the Rube Goldberg Software!\nWaiting for keypress..."
-os.system("pause")
+raw_input("Press [Enter] to send the signal.")
 print "Key pressed. Now sending signal..."
 s.connect((host, port))
 s.close
+print "Signal sent. \nSoftware by Berbawy Makers, (C) 2016."
