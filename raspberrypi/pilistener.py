7c577bac9c1e342c605ff8daa6d09bce82ea8ad4
AppleCrazy <applecrazy@users.noreply.github.com>
2016-02-25 07:48:14 -0800

    Update pilistener.py

--- raspberrypi/pilistener.py
+++ raspberrypi/pilistener.py
@@ -1,7 +1,23 @@
 import socket               # Import socket module
+import time # Import the time module
+import RPi.GPIO as GPIO
+
+serDelay = 0.0015 # The delay between the high and low pulses
+
+# Set up GPIO
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7,GPIO.OUT)
 
 def move():
-   print "moved."
+   try:
+      GPIO.output(7,1)
+      time.sleep(serDelay)
+      GPIO.output(7,0)
+      print "moved."
+   except:
+      print "Unexpected error:", sys.exc_info()[0]
+      move()
+      raise
 s = socket.socket()         # Create a socket object
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Tells the system to okay the reuse of a port
 host = socket.gethostname() # Get local machine name
