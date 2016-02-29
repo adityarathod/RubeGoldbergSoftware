efdc8b97ae7dda230c57257d35fe9bdc0044f11a
AppleCrazy <applecrazy@users.noreply.github.com>
2016-02-29 07:38:19 -0800

    Update pisender.py

--- raspberrypi/pisender.py
+++ raspberrypi/pisender.py
@@ -2,13 +2,13 @@ import os
 
 import socket
 s = socket.socket()
-# Localhost for testing purposes
+# Localhost for testing purposes, please replace "socket.gethostname()" with the IP address of the next school, in quotes.
 host = socket.gethostname() #"127.0.0.1"
 # Need to figure out what port all school networks DON'T block.
 port = 238
-print "Welcome to the Rube Goldberg Software!\nWaiting for keypress..."
+print "Welcome to the Rube Goldberg Software, created by AppleCrazy.\nWaiting for keypress..."
 raw_input("Press [Enter] to send the signal.")
 print "Key pressed. Now sending signal..."
 s.connect((host, port))
 s.close
-print "Signal sent. \nSoftware by Berbawy Makers, (C) 2016."
+print "Signal sent sucessfully."
