2641c2ae9b4524b1d9b0f710134ed46a8bba297d
AppleCrazy <archronicle7@gmail.com>
2016-04-28 16:14:32 +0000

    Added dashboard comms scripts and crontab integration

---
+++ raspberrypi/dashboardcomms.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+import time
+import os
+n = ""
+with open("pinum.txt","read") as f:
+    n = f.read()
+for i in range(59):
+    os.system("curl --data \"id=" + n + "\" https://rubedashboard.herokuapp.com/pistate.php")
+    print "iteration " + str(i)
+    time.sleep(0.8)
---
+++ raspberrypi/updatescripts/dashboardupdate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Rube Software Update 1.1.0_86"
+echo "Creating log file at ~/commslog.txt ..."
+touch /home/pi/commslog.txt
+echo "Adding script to cron (task scheduler)...please wait."
+(crontab -l 2>/dev/null; echo "* * * * * python /home/pi/rubegoldberg/RubeGoldbergSoftware/raspberrypi/dashboardcomms.py >> /home/pi/commslog.txt 2>&1") | crontab -
+echo "Sucessfully updated."
