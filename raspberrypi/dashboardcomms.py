9784ac7a05c1e1a9ab61372816c299e4f03c6ef6
AppleCrazy <applecrazy@users.noreply.github.com>
2016-05-02 15:50:59 -0700

    Update dashboardcomms.py

--- raspberrypi/dashboardcomms.py
+++ raspberrypi/dashboardcomms.py
@@ -2,7 +2,7 @@
 import time
 import os
 n = ""
-with open("pinum.txt","read") as f:
+with open("/home/pi/pinum.txt","read") as f:
     n = f.read()
 for i in range(59):
     os.system("curl --data \"id=" + n + "\" https://rubedashboard.herokuapp.com/pistate.php")
