1097b07b4a7630e27466130f229f24e143064fc0
AppleCrazy <applecrazy@users.noreply.github.com>
2016-05-02 15:57:09 -0700

    Update dashboardupdate.sh

--- raspberrypi/updatescripts/dashboardupdate.sh
+++ raspberrypi/updatescripts/dashboardupdate.sh
@@ -2,6 +2,7 @@
 echo "Rube Software Update 1.1.0_86"
 echo "Creating log file at ~/commslog.txt ..."
 touch /home/pi/commslog.txt
+touch /home/pi/pinum.txt
 echo "Adding script to cron (task scheduler)...please wait."
 (crontab -l 2>/dev/null; echo "* * * * * python /home/pi/rubegoldberg/RubeGoldbergSoftware/raspberrypi/dashboardcomms.py >> /home/pi/commslog.txt 2>&1") | crontab -
 echo "Sucessfully updated."
